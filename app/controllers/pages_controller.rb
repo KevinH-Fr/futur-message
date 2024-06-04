3313c78509a5dfcb6d6c9cc9c5a45126876f8679
KevinH-Fr <kevin.hoffman.france@gmail.com>
2024-06-04 17:14:51 +0200

    mise en forme devise - navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,7 @@ class PagesController < ApplicationController
         @messages = Message.all
     end
 
+    def about
+    end
+
 end
--- app/helpers/nav_helper.rb
+++ app/helpers/nav_helper.rb
@@ -1,14 +1,12 @@
 module NavHelper
-    def nav_link(path, name, icon_class, options = {})
-      classes = ["nav-item text-center m-2"]
-      classes << "active" if current_page?(path)
-  
-      content_tag :li, class: classes do
-        link_to path, options.merge({ class: "text-decoration-none" }) do
-          concat content_tag(:i, "", class: "fa fa-xl bg-secondary #{icon_class} m-2")
-          concat content_tag(:span, name, class: "text-dark fw-bold")
-        end
+  def nav_link(path, name, icon_class, options = {})
+    classes = ["nav-item text-center m-2"]  # Add "bg-gray-400" for grey background
+
+    content_tag :li, class: classes do
+      link_to path, options.merge({ class: "navbar-item #{'selected-navbar' if current_page?(path)}" }) do
+        concat content_tag(:i, "", class: "fa fa-xl #{icon_class} m-2")
+        concat content_tag(:span, name, class: "fw-bold")
       end
     end
   end
-  
+end
