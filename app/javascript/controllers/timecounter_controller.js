fb7de2335277ce3aca444cdefbba747a2d6a4443
KevinH-Fr <kevin.hoffman.france@gmail.com>
2024-06-06 18:51:13 +0200

    test remove assets

--- app/javascript/controllers/timecounter_controller.js
+++ app/javascript/controllers/timecounter_controller.js
@@ -1,26 +1,22 @@
-import { Controller } from "@hotwired/stimulus";
+// app/javascript/controllers/time_controller.js
+import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["counter"];
+  static targets = [ "counter" ]
 
   connect() {
     console.log("timecounter connected");
-    this.updateTime();
-    this.interval = setInterval(() => this.updateTime(), 1000);
+    this.updateTime()
+    this.interval = setInterval(() => this.updateTime(), 1000)
   }
 
   disconnect() {
-    clearInterval(this.interval);
+    clearInterval(this.interval)
   }
 
   updateTime() {
-    const currentTime = new Date();
+    const currentTime = new Date()
     console.log("current time :" + currentTime);
-    
-    const hours = String(currentTime.getUTCHours()).padStart(2, "0");
-    const minutes = String(currentTime.getUTCMinutes()).padStart(2, "0");
-    const seconds = String(currentTime.getUTCSeconds()).padStart(2, "0");
-
-    this.counterTarget.innerText = "test value";
+    this.counterTarget.innerText = currentTime.toLocaleTimeString()
   }
 }
