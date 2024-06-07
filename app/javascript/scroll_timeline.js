5419ae34a33eb16f07641227d9c46da115490af8
KevinH-Fr <kevin.hoffman.france@gmail.com>
2024-06-08 01:01:41 +0200

    working exemple animation on scroll on multiple pages

--- app/javascript/scroll_timeline.js
+++ app/javascript/scroll_timeline.js
@@ -22,4 +22,36 @@ document.addEventListener('DOMContentLoaded', () => {
     );
   });
   
+
+    // Apply animation to elements with the class 'scroll-animate'
+    document.querySelectorAll('.scroll-appear').forEach((element) => {
+        element.animate(
+          {
+            opacity: [0, 1], // Appear and disappear
+        },
+          {
+            duration: 2,
+            fill: 'both',
+            timeline: scrollTimeline,
+          }
+        );
+    });
+
+
+    // Apply animation to elements with the class 'scroll-slide'
+    document.querySelectorAll('.scroll-slide-right').forEach((element) => {
+        element.animate(
+            {
+            transform: ['translateX(-100%)', 'translateX(0%)'], // Slide from left to right
+            },
+            {
+            duration: 1, // The duration will be determined by the scroll
+            fill: 'both',
+            timeline: scrollTimeline,
+            }
+        );
+    });
+
+
+
 });
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
 
+  get "home", to: "pages#home"
+  get "about", to: "pages#about"
+
   root "pages#home"
+
 
 end
