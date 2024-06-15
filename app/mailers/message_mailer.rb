a3cf74afad6ec1709c6d1023348ba1eaedf20233
KevinH-Fr <kevin.hoffman.france@gmail.com>
2024-06-15 17:40:52 +0200

    btn open sous condition - sans transition - correction email

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,30 +1,31 @@
 class MessageMailer < ApplicationMailer
+  default from: 'contact.futurmessage@gmail.com'
+  BASE_URL = 'https://futur-message-ecb35003e1a0.herokuapp.com/'
 
-    default from: 'your_email@example.com'
-    
-    def sender_scheduled_email(message)
-      @message = Message.find(message)
-      @url = "https://futur-message-ecb35003e1a0.herokuapp.com/"
-      mail(to: @message.sender.email, subject: "L'envoi de votre message a bien été programmé")
-    end
+  def sender_scheduled_email(message_id)
+    setup_message_and_url(message_id)
+    mail(to: @message.sender.email, subject: "L'envoi de votre message a bien été programmé")
+  end
 
-    def sender_delivered_email(message)
-      @message = Message.find(message)
-      @url = "https://futur-message-ecb35003e1a0.herokuapp.com/"
-      subject = 
-      mail(to: @message.sender.email, subject: 'Votre message a bien été envoyé')
-    end
+  def sender_delivered_email(message_id)
+    setup_message_and_url(message_id)
+    mail(to: @message.sender.email, subject: 'Votre message a bien été envoyé')
+  end
 
-    def receiver_scheduled_email(message)
-      @message = Message.find(message)
-      @url = "https://futur-message-ecb35003e1a0.herokuapp.com/"
-      mail(to: @message.receiver.email, subject: "Vous message pour le futur")
-    end
+  def receiver_scheduled_email(message_id)
+    setup_message_and_url(message_id)
+    mail(to: @message.receiver.email, subject: "Votre message pour le futur")
+  end
 
-    def receiver_delivered_email(message)
-      @message = Message.find(message)
-      @url = "https://futur-message-ecb35003e1a0.herokuapp.com/"
-      mail(to: @message.receiver.email, subject: 'Votre nouveau message est lisible')
-    end
+  def receiver_delivered_email(message_id)
+    setup_message_and_url(message_id)
+    mail(to: @message.receiver.email, subject: 'Votre nouveau message est lisible')
+  end
 
+  private
+
+  def setup_message_and_url(message_id)
+    @message = Message.find(message_id)
+    @url = BASE_URL
+  end
 end
